c8bcbd36d889dd01cb9beee91b6612ffd1bc3aa0
fvzzini <b.knvpik@gmail.com>
2021-03-01 18:37:14 +0100

    Initial commit, add basic structure, add styled landing page

---
+++ src/components/LandingPage.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import triangles from '../triangles.svg';
+import arrow from '../arrow.svg';
+
+const LandingPage = () => {
+    return (
+        <section>
+            <div id="landing-page">
+                <header>
+                    <i className="fab fa-linkedin"></i>
+                    <i className="fab fa-facebook-square"></i>
+                    <i className="fab fa-github-square"></i>
+                </header>
+                <div id="landing-wrapper">
+                    <div id="landing-left">
+                        <p id="hi">Hi, my name is</p>
+                        <h1 id="my-name">Bartosz.</h1>
+                        <p id="whoami">
+                            &#123;
+                            <br/>
+                                <div>I am learning to</div>
+                                <div>become a <span id="red">front</span>-<span id="navy">end</span> dev</div>
+                            &#125;
+                        </p>
+                    </div>
+                    <div id="landing-right">
+                        <img src={triangles} alt=""></img>
+                        <button>contact me</button>
+                    </div>
+                </div>
+                <footer>
+                    <img src={arrow} alt=""></img>
+                </footer>
+        </div>
+      </section>
+    );
+};
+
+export default LandingPage;
